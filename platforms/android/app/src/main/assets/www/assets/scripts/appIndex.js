5b12dea8b85f6b038db882665ab539df5b746687
dyprazst <romadhanedy@outlook.com>
2020-02-17 14:49:26 +0700

    FE Landing Added

--- platforms/android/app/src/main/assets/www/assets/scripts/appIndex.js
+++ platforms/android/app/src/main/assets/www/assets/scripts/appIndex.js
@@ -1,16 +1,23 @@
 $('#app').hide()
 let localDB = window.localStorage
 
+
+function pushPage(__url){
+    setTimeout(function(){
+        window.location = __url
+    }, 500)
+}
+
 if(localDB.getItem("sesIntro") != 1){
-    function pushPage(__url){
-        setTimeout(function(){
-            window.location = __url
-        }, 500)
-    }
     pushPage("intro/index.html")
 }
 else{
-    setTimeout(function(){
-       $('#app').fadeIn("slow")
-    },200)
+    if(localDB.getItem("sesLogged") != 1){
+        pushPage("login.html")
+    }
+    else{
+        setTimeout(function(){
+           $('#app').fadeIn("slow")
+        },200)
+    }
 }
